6e11ad0c3023c12459c6fd24407acdad6a64b937
Roga Raccoon <7654595@gmail.com>
2025-10-05 21:08:22 +0300

    fin

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿
+using System;
+using System.Diagnostics;
+
+class SubstringFinder
+{
+    public int FindIndexOfSubstring(string text, string pattern)
+    {
+        for (int i = 0; i <= text.Length - pattern.Length; i++)
+        {
+            if (IsMatch(text, pattern, i))
+                return i;
+        }
+        return -1;
+    }
+
+    private bool IsMatch(string text, string pattern, int startIndex)
+    {
+        for (int j = 0; j < pattern.Length; j++)
+        {
+            if (text[startIndex + j] != pattern[j])
+                return false;
+        }
+        return true;
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        string text = "hello world this is a test string";
+        string pattern = "test";
+        var finder = new SubstringFinder();
+
+        var sw = Stopwatch.StartNew();
+        int index = finder.FindIndexOfSubstring(text, pattern);
+        sw.Stop();
+
+        Console.WriteLine($"Индекс подстроки: {index}");
+        Console.WriteLine($"Время: {sw.ElapsedTicks} тиков");
+    }
+}
---
+++ Tests/UnitTest1.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+
+[TestFixture]
+public class SubstringFinderTests
+{
+    [Test]
+    public void Test_SubstringFound()
+    {
+        var finder = new SubstringFinder();
+        int result = finder.FindIndexOfSubstring("hello world this is a test string", "test");
+        Assert.That(result, Is.EqualTo(22));
+    }
+
+    [Test]
+    public void Test_SubstringNotFound()
+    {
+        var finder = new SubstringFinder();
+        int result = finder.FindIndexOfSubstring("abcdef", "xyz");
+        Assert.That(result, Is.EqualTo(-1));
+    }
+}
